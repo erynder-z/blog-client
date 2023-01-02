a8c9561084fc16fe6dabb90fecd7a81ee020fc89
erynder-z <stfn.bgr@gmail.com>
2023-01-02 18:57:52 +0100

    Update main layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,23 @@ import LatestPosts from './components/Main/LatestPosts/LatestPosts';
 function App() {
   return (
     <div className="app-container">
-      <nav>
-        <Navbar />
-      </nav>
       <div className="main-container">
-        <main className="main">
+        <nav>
+          <Navbar />
+        </nav>
+        <main>
           <Routes>
             <Route path="/" element={<Navigate replace to="/latest" />}></Route>
             <Route path="/all" element={<AllPosts />}></Route>
             <Route path="/latest" element={<LatestPosts />}></Route>
           </Routes>
         </main>
-        <aside>
-          <Sidebar />
-        </aside>
       </div>
+      <aside>
+        <div className="side-container">
+          <Sidebar />
+        </div>
+      </aside>
     </div>
   );
 }
