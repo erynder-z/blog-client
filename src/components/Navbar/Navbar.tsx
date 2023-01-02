89af4e63742097a336364784c935e951c910cb3d
erynder-z <stfn.bgr@gmail.com>
2023-01-02 18:58:17 +0100

    Add navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,5 +2,23 @@ import React from 'react';
 import './Navbar.css';
 
 export default function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <h1 className="nav-title">Blog</h1>
+      <div className="nav-list">
+        <a href="/all" className="nav-list-item">
+          All
+        </a>
+        <a href="/latest" className="nav-list-item">
+          Latest
+        </a>
+        <a href="/tags" className="nav-list-item">
+          Tags
+        </a>
+        <a href="/about" className="nav-list-item">
+          About
+        </a>
+      </div>
+    </div>
+  );
 }
