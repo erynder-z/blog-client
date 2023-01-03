7ba975d0a3a22027a2bd6c0ce5a2ba16ca7c8b10
erynder-z <stfn.bgr@gmail.com>
2023-01-03 20:21:57 +0100

    Add about component

---
+++ src/components/Main/About/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css';
+
+export default function About() {
+  return (
+    <main className="about">
+      <h1>About this blog</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni sapiente illo esse fugit
+        ullam reiciendis nostrum veniam alias quae quod.
+      </p>
+      <ul>
+        <li>Lorem, ipsum.</li> <li>Lorem, ipsum.</li> <li>Lorem, ipsum.</li> <li>Lorem, ipsum.</li>
+      </ul>
+    </main>
+  );
+}
