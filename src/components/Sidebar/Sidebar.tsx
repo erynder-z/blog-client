bf64a6e729a4ae2f6ca54ff5bd01a9ebf8433200
erynder-z <stfn.bgr@gmail.com>
2023-01-05 13:25:48 +0100

    Display tags in sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,46 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { ITag } from '../../Interfaces/Tag';
 import './Sidebar.css';
 
-export default function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+interface Props {
+  setActiveTag: Dispatch<SetStateAction<ITag | null>>;
+}
+
+export default function Sidebar({ setActiveTag }: Props) {
+  const [tagList, setTagList] = useState<ITag[]>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchTagListData = async () => {
+    try {
+      const res = await fetch('http://localhost:8000/api/tags');
+      const data = await res.json();
+      setTagList(data.tag_list);
+    } catch (err: any) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTagListData();
+  }, []);
+
+  return (
+    <div className="sidebar">
+      <h1 className="side-tags-heading">All tags</h1>
+      <ul className="side-tag-list">
+        {tagList?.map((tag: ITag) => (
+          <li
+            key={tag._id.toString()}
+            className="side-tag-list-item"
+            onClick={() => {
+              setActiveTag(tag);
+            }}>
+            {tag.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
