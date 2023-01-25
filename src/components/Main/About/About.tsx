e4c08c2bb89e6d8617bce0fcd2dfb7184b2b5584
erynder-z <stfn.bgr@gmail.com>
2023-01-25 21:20:55 +0100

    Add about page

--- src/components/Main/About/About.tsx
+++ src/components/Main/About/About.tsx
@@ -3,15 +3,26 @@ import './About.css';
 
 export default function About() {
   return (
-    <main className="about">
+    <main className="about-container">
       <h1>About this blog</h1>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni sapiente illo esse fugit
-        ullam reiciendis nostrum veniam alias quae quod.
+        Welcome to my blog! I am Stefan, a self-taught developer and writer with a passion for
+        learning and sharing my knowledge with others. I have created this platform to share my
+        thoughts, ideas, and experiences on a variety of topics that interest me.
       </p>
-      <ul>
-        <li>Lorem, ipsum.</li> <li>Lorem, ipsum.</li> <li>Lorem, ipsum.</li> <li>Lorem, ipsum.</li>
-      </ul>
+      <p>
+        I built this blog using React on the frontend and created a self-coded REST API with Express
+        and MongoDB on the backend. I have also coded a CMS to provide the content you see here.
+      </p>
+      <p>
+        I believe in providing my readers with a seamless and personalized experience, that's why
+        I've integrated features like commenting, keyword search, and article filtering by tags. My
+        readers can also change the theme of the website to suit their preferences.
+      </p>
+      <p>
+        I hope you find my blog informative, engaging, and thought-provoking. I welcome your
+        comments and feedback and look forward to hearing from you. Thank you for visiting my blog!
+      </p>
     </main>
   );
 }
