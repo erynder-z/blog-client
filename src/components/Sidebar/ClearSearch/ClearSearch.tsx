321461ab5f282035a06dc9776250ffb06e44a7ca
erynder-z <stfn.bgr@gmail.com>
2023-02-04 17:44:57 +0100

    navigate to latest articles after clear

--- src/components/Sidebar/ClearSearch/ClearSearch.tsx
+++ src/components/Sidebar/ClearSearch/ClearSearch.tsx
@@ -2,11 +2,14 @@ import React, { useContext } from 'react';
 import './ClearSearch.css';
 import { FaUndoAlt } from 'react-icons/fa';
 import FilterContext from '../../../contexts/FilterContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function ClearSearch() {
   const { setFilter } = useContext(FilterContext);
+  const navigate = useNavigate();
   const handleClearSearch = () => {
     setFilter(null);
+    navigate('/latest');
   };
 
   return (
