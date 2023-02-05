0e1a1f9422d9423d9d416b8cd6ddc9e03755a0e9
erynder-z <stfn.bgr@gmail.com>
2023-02-05 20:48:57 +0100

    Fix random article initial load

--- src/components/Sidebar/ReadRandomArticle/ReadRandomArticle.tsx
+++ src/components/Sidebar/ReadRandomArticle/ReadRandomArticle.tsx
@@ -4,36 +4,39 @@ import { FaRandom } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 export default function ReadRandomArticle() {
-  const [articleId, setArticleId] = useState<string | null>('');
+  const [articleId, setArticleId] = useState<string | null>(null);
   const [buttonClicked, setButtonClicked] = useState(false);
+
+  const handleClick = () => {
+    setButtonClicked(true);
+  };
+
+  const fetchArticleID = async () => {
+    try {
+      const serverURL = import.meta.env.VITE_SERVER_URL;
+      const res = await fetch(`${serverURL}/api/articles/random`);
+      if (res.ok) {
+        const data = await res.json();
+        setArticleId(data.articleId);
+        setButtonClicked(false);
+      } else {
+        throw new Error(`Server returned ${res.status} ${res.statusText}`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
     if (!buttonClicked) {
       return;
     }
-
-    const fetchData = async () => {
-      try {
-        const serverURL = import.meta.env.VITE_SERVER_URL;
-        const res = await fetch(`${serverURL}/api/articles/random`);
-        if (res.ok) {
-          const data = await res.json();
-          setArticleId(data.articleId);
-          setButtonClicked(false);
-        } else {
-          throw new Error(`Server returned ${res.status} ${res.statusText}`);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    fetchData();
+    fetchArticleID();
   }, [buttonClicked]);
 
-  const handleClick = () => {
-    setButtonClicked(true);
-  };
+  useEffect(() => {
+    fetchArticleID();
+  }, []);
 
   return (
     <Link to={`/article/${articleId}`} className="randomArticleBtn" onClick={handleClick}>
